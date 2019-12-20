bf3bd5a7c3b53dce210b45a09c64c8871342affe
jeevan raj <jeevan.sr@lnttechservices.com>
2019-12-20 17:47:43 +0530

    sign up form with modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "./auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'looplab';
+export class AppComponent implements OnInit {
+  // title = "looplab";
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  loginform: FormGroup;
+  registerform: FormGroup;
+  isLoading: boolean = false;
+  submitted: boolean = false;
+  error: string = null;
+  IsmodelShow: boolean = false;
+
+  ngOnInit() {
+    this.registerform = this.formBuilder.group({
+      // username: ["", Validators.required],
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+        ]
+      ],
+      password: ["", [Validators.required, Validators.minLength(6)]]
+      // confirmPassword: ["", [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  get registerControl() {
+    return this.registerform.controls;
+  }
+  onRegister(form: FormGroup) {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (!this.registerform.valid) {
+      return;
+    }
+
+    this.isLoading = true;
+    // const username = registerForm.value.username;
+    const email = form.value.email;
+    const password = form.value.password;
+
+    // const confirmpassword = registerForm.value.confirmpassword;
+    this.authService.signup(email, password).subscribe(
+      resData => {
+        console.log("server response data: ", resData);
+        this.isLoading = false;
+        this.router.navigate(["/dashboard"]);
+      },
+      errorResponse => {
+        console.log("error response data: ", errorResponse);
+        this.error = errorResponse;
+        this.isLoading = false;
+      }
+    );
+  }
+  onLogin(loginform: FormGroup) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AuthService } from "./auth.service";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
